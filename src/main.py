3cf8b30c57a96a5c305e98085ebb3bef25956f9b
afiaisrat43-debug <afiaisrat43@gmail.com>
2026-08-02 22:42:44 +0600

    Adding Multiply function to calculator

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from datetime import date
-from utils import add,subtract
+from utils import add,subtract,multiply
 
 print("My name is Afia")
 print("Today is ", date.today())
@@ -7,3 +7,4 @@ print("Today is ", date.today())
 print("\nCalculator Test")
 print("4+5=", add(4,5))
 print("4+5=", subtract(4,5))
+print("4*5=", multiply(4,5))
