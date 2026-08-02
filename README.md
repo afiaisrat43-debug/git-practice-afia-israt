cd4e4955ae5fdcbbd3c46d746620524270cf4232
afiaisrat43-debug <afiaisrat43@gmail.com>
2026-08-02 21:27:41 +0600

    Intialize commit: project structure and basicprogram

---
+++ README.md
@@ -0,0 +1,3 @@
+#Git Practice Project
+Afia Israt
+This project is for learning Git and GitHub Basic
---
+++ src/main.py
@@ -0,0 +1,9 @@
+from datetime import date
+from utils import add,subtract
+
+print("My name is Afia")
+print("Today is ", date.today())
+
+print("\nCalculator Test")
+print("4+5=", add(4,5))
+print("4+5=", subtract(4,5))
